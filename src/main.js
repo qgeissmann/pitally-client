8dac8f66cc25a4828fedc55b901cd24b586fd198
Quentin Geissmann <qgeissmann@gmail.com>
2019-01-28 13:46:31 -0800

    include material design icons (remote otherwise)

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./routes";
